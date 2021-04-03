156b77c25d654428f5bb074cbb1d2c5094f1b7dc
Alexrjj <alexsoares2517@gmail.com>
2021-04-03 17:37:45 -0300

    Altera localização da pasta js

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+import importScript from "../src/utils/importScript";
 import "./App.css";
 
 export default function Header() {
@@ -23,6 +25,30 @@ export default function Header() {
     borderRadius: "15px",
   };
 
+  importScript("src/js/jquery.js");
+  importScript("src/js/jquery.easing.min.js");
+  importScript("src/js/bootstrap.min.js");
+  importScript("src/js/smoothscroll.js");
+  importScript("src/js/classie.js");
+  importScript("src/js/jquery.easytabs.min.js");
+  importScript("src/js/piechart.js");
+  importScript("src/js/particles.js");
+  importScript("src/js/owl.carousel.min.js");
+  importScript("src/js/jqBootstrapValidation.js");
+  importScript("src/js/scripts.js");
+
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+
+  //   script.src = "https://use.typekit.net/foobar.js";
+  //   script.async = true;
+
+  //   document.body.appendChild(script);
+
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
   return (
     <div>
       {/* <!-- PRELOADER --> */}
